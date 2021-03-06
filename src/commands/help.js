39757a41648d56bc4c8d6b2a5c4ea9b1bc48f6df
Diego Winter <diego@diegowinter.dev>
2021-03-05 21:44:00 -0300

    feat: add new commands to help command
    - Add information about now playing, remove, seek and lyrics command.

--- src/commands/help.js
+++ src/commands/help.js
@@ -10,10 +10,14 @@ module.exports = function help(message) {
         .addField('Commands',
             "\`-p (or -play) <song name/link or playlist link>\`: Play/add to queue a song/playlist " +
                 "(YouTube and Spotify links are accepted)\n" +
+            "\`-np (or -nowplaying)\`: Show information about current song\n" +
             "\`-q (or -queue)\`: Show current queue\n" +
             "\`-sk (or -skip) <position in queue (optional)>\`: Skip to next song or to given index\n" +
             "\`-st (or -stop)\`: Stop playback\n" +
-            "\`-v (or -volume) <value between 0 and 100>\`: Change volume\n"
+            "\`-v (or -volume) <value between 0 and 100>\`: Change volume\n"+
+            "\`-rm (or -remove) <position in queue>\`: Remove a song in queue\n"+
+            "\`-se (or -seek) <time in format mm:ss>\`: Seek to a position in the song\n"+
+            "\`-l (or -lyrics)\`: Get the lyrics of the song"
         )
         .setFooter(`LegendaryDJ v${process.env.npm_package_version}`)
     );
