64e1134c203aaa7d499c9e16a9ffb46455a1d777
Diego Winter <diego@diegowinter.dev>
2021-11-01 19:40:27 -0300

    fix: change help for volume command

--- src/commands/help.js
+++ src/commands/help.js
@@ -12,10 +12,10 @@ module.exports = function help(message) {
       "\`-q (or -queue)\`: Show current queue\n" +
       "\`-sk (or -skip) <position in queue (optional)>\`: Skip to next song or to given index\n" +
       "\`-st (or -stop)\`: Stop playback\n" +
-      "\`-v (or -volume) <value between 0 and 100>\`: Change volume\n"+
+      "\`-v (or -volume) <value between 0 and 200>\`: Change volume\n"+
       "\`-rm (or -remove) <position in queue>\`: Remove a song in queue\n"+
       "\`-se (or -seek) <time in format mm:ss>\`: Seek to a position in the song\n"+
-      "\`-l (or -lyrics) <song name (optional)> \`: Get the lyrics of a song"
+      "\`-l (or -lyrics) <song name (optional)>\`: Get the lyrics of a song"
     )
     .setFooter(`LegendaryDJ v${process.env.npm_package_version}`);
 }
